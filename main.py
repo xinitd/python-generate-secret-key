3bf24ab260b69c9fb03253f31b2feccbfa24d86f
Shakhzhakhan Maxudbek <xinit@args.tech>
2023-12-06 04:02:26 +0600

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_random_string(length=12,
+                      allowed_chars='abcdefghijklmnopqrstuvwxyz'
+                                    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    return ''.join(random.choice(allowed_chars) for i in range(length))
+
+def get_random_secret_key():
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    return get_random_string(50, chars)
+
+print(get_random_secret_key())
